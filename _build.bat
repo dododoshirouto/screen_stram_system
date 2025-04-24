daad22c5cca3e87483368904a78cd1ba9d8f0544
dododoshirouto <yfkf19961009@gmail.com>
2025-04-25 08:11:31 +0900

    client_secretのJSONファイルをビルドに追加

--- _build.bat
+++ _build.bat
@@ -5,6 +5,7 @@ if exist dist\screen_stream.exe (
 venv\Scripts\python.exe -m PyInstaller --onefile --noconsole ^
     --name "screen_stream" ^
     --icon "icons/on.ico" ^
+    --add-data "client_secret_458758854605-ihia8ttepcfjeab3k80lk1rc40dttso9.apps.googleusercontent.com.json;." ^
     --add-data "icons/off.png;icons" ^
     --add-data "icons/on.png;icons" ^
     --add-data "icons/mosaic.png;icons" ^
